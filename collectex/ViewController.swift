b35c1ddf15b37e78761f094d606060ca9bb64d6e
Ventuno Technologies <ventuno@ventunoLunar.local>
2019-02-26 13:03:26 +0530

    Index selction done

--- collectex/ViewController.swift
+++ collectex/ViewController.swift
@@ -13,23 +13,39 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
 {
     
     var arr:[String] = ["India","Australia","England","Newzealand","Pakistan"]
-    
+    var previous_index:IndexPath?
     
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return arr.count
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell
-        cell?.list.text = arr[indexPath.row]
-            //String(indexPath.row)
-        //cell!.list(with: arr[indexPath.row])
+        
+        cell?.list.text = String(indexPath.row)
+       
+            
         return cell!
     }
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Index \(indexPath.row)")
+        
+        //Fallback Background
+        let FallbackCell: UICollectionViewCell? = collectionView.cellForItem(at: previous_index ?? indexPath)
+        FallbackCell?.layer.backgroundColor = UIColor.white.cgColor
+        
+        //Chnage Current Background
+        let cell: UICollectionViewCell? = collectionView.cellForItem(at: indexPath)
+        //cell?.layer.cornerRadius = cellWidth / 2.0
+        cell?.layer.backgroundColor = UIColor.red.cgColor
+        
+        self.previous_index = indexPath
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
